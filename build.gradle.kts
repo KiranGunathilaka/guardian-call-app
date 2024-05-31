fe9417ae49ab81c57894e28dac54b2596f0464ac
KiranGunathilaka <kirangunathilaka@gmail.com>
2024-05-31 08:56:07 +0530

    Created the login UI as per the figma design.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
     dependencies {
         classpath(libs.google.services)
-        classpath("com.google.gms:google-services:4.2.0")
+        classpath("com.google.gms:google-services:4.3.10")
 
     }
 }
